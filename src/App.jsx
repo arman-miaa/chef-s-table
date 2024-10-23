c11bfa2b21b4f294c0990847b47370d8b23f4b21
Arman <mdarman365506@gmail.com>
2024-10-23 14:18:41 +0600

    added header and banner section

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Banner from "./components/Banner";
+import Header from "./components/Header";
+import OurRecipes from "./components/OurRecipes";
+
+
+const App = () => {
+  return (
+    <div className="container mx-auto px-4">
+      {/* Header */}
+      <Header></Header>
+      {/* Banner */}
+      <Banner></Banner>
+      {/* Our Recipes Section */}
+      <OurRecipes></OurRecipes>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/OurRecipes.jsx
@@ -0,0 +1,14 @@
+
+
+const OurRecipes = () => {
+    return (
+        <div className="text-center my-8 w-8/12 mx-auto space-y-4">
+            <h1 className="font-semibold text-5xl text-gray-800">Our Recipes</h1>
+            <p className="text-base text-gray-600">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur facilis provident impedit sit, corporis saepe quod sint quisquam aspernatur eligendi fuga porro. Dolor cumque et veritatis enim inventore officiis mollitia.
+            </p>
+        </div>
+    );
+};
+
+export default OurRecipes;
